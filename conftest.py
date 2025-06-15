68f5bbc34622b7879c8effa8a36fb58856a99b94
alvardall <alvardtel@gmail.com>
2025-06-16 03:34:05 +0400

    first commit

--- conftest.py
+++ conftest.py
@@ -14,10 +14,10 @@ def test_driver():
     # Run Chrome in headless mode
     options = webdriver.ChromeOptions()
     options.add_argument('--headless')
-    options.add_argument('--no-sandbox')
-    options.add_argument('--disable-dev-shm-usage')
-    
     driver = webdriver.Chrome(options=options)
+    
+    #driver = webdriver.Chrome()
+    driver.maximize_window()
     yield driver
     driver.quit()
 
